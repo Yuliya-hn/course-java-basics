e6937f6f563e967b851942828e5fed2502bc6dc5
yhankovich <yuliyagnk@gmail.com>
2020-10-10 01:02:52 +0300

    completed task2

--- src/main/java/com/rakovets/course/javabasics/practice/loops/Task02.java
+++ src/main/java/com/rakovets/course/javabasics/practice/loops/Task02.java
@@ -25,5 +25,22 @@ public class Task02 {
         //TODO
         // Код, решающий задачу пишем ниже, при этом используяся переменные объявленные выше (их можно изменять)
         // Для проверки решения необходимо запустить @Test для данного class (в директории test)
+        printGeneratedArray(rows, headerRowEnable);
+    }
+    static void printGeneratedArray(int countRows, boolean headerRowEnable) {
+        int rowNumber = 1;
+        for (int i = 0; i < countRows; i++) {
+            if (i == 0) {
+                if (headerRowEnable) {
+                    System.out.print("\n");
+                    i++;
+                }
+                System.out.print(rowNumber);
+            } else {
+                System.out.print("\n" + rowNumber);
+            }
+            rowNumber++;
+
+        }
     }
 }

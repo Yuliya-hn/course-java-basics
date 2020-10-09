f84a9e96b628793a5fcde4e8ec9692430842ecf3
yhankovich <yuliyagnk@gmail.com>
2020-10-10 01:07:51 +0300

    completed task6

--- src/main/java/com/rakovets/course/javabasics/practice/loops/Task06.java
+++ src/main/java/com/rakovets/course/javabasics/practice/loops/Task06.java
@@ -1,5 +1,6 @@
 package com.rakovets.course.javabasics.practice.loops;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 /**
@@ -21,7 +22,15 @@ public class Task06 {
         long amount = (args.length != 1 ? scanner.nextLong() : Long.parseLong(args[0]));
 
         //TODO
-        // Код, решающий задачу пишем ниже, при этом используяся переменные объявленные выше (их можно изменять)
-        // Для проверки решения необходимо запустить @Test для данного class (в директории test)
+        // Код, решающий задачу пишем ниже, при этом используяся переменные объявленные выше (их\ можно изменять)
+        // Для проверки решени;я необходимо запустить @Test для данного class (в директории test)
+        String amountString = new StringBuilder(String.valueOf(amount)).reverse().toString();
+        char[] reverseArray1 = amountString.toCharArray();
+        for (int i = reverseArray1.length; i > 0; i--) {
+            if (i % 3 == 0) {
+                System.out.print(" ");
+            }
+            System.out.print(reverseArray1[i-1]);
+        }
     }
 }

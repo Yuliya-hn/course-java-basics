374067735dccbd3a03168b72b8ea6a489f1c7e2d
yhankovich <yuliyagnk@gmail.com>
2020-10-10 01:04:59 +0300

    completed task4, swap actual and expected result in test

--- src/main/java/com/rakovets/course/javabasics/practice/loops/Task04.java
+++ src/main/java/com/rakovets/course/javabasics/practice/loops/Task04.java
@@ -27,5 +27,15 @@ public class Task04 {
         //TODO
         // Код, решающий задачу пишем ниже, при этом используяся переменные объявленные выше (их можно изменять)
         // Для проверки решения необходимо запустить @Test для данного class (в директории test)
+        double totalHP = healthPoints;
+        double hours = 1;
+
+        while ( hours <= hoursAfterRespawn ){
+            double percent = totalHP * (regenerationPercentFromCurrentHealth / 100);
+            totalHP += percent;
+            hours ++;
+        }
+
+        System.out.println( (int) Math.floor(totalHP));
     }
 }

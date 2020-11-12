a0280f24dd9e84db776d6eeb44fcf9a04004e78b
yhankovich <yuliyagnk@gmail.com>
2020-11-12 13:35:22 +0300

    second attempt to edit Collection task

--- src/main/java/com/rakovets/course/javabasics/practice/oop/inheritanceandpolymorphism/ground/stringCollectionUtil/StringCollectionUtil.java
+++ src/main/java/com/rakovets/course/javabasics/practice/oop/inheritanceandpolymorphism/ground/stringCollectionUtil/StringCollectionUtil.java
@@ -1,15 +1,19 @@
 package com.rakovets.course.javabasics.practice.oop.inheritanceandpolymorphism.ground.stringCollectionUtil;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 
 public class StringCollectionUtil {
     public void resetWordsByLength(Collection<String> strings, int length) {
-        ArrayList<String> localArrayList = (ArrayList<String>) strings;
+        ArrayList<String> localArrayList = new ArrayList<>();
+        localArrayList.addAll(strings);
         for (int i = 0; i < strings.size(); i++) {
             if (localArrayList.get(i).length() == length) {
                 localArrayList.add(i, "*");
             }
         }
+        strings.clear();
+        strings.addAll(localArrayList);
     }
 
     public void removeWordsByLength(Collection<String> strings, int length) {
